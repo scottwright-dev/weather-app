741af26cc7ef94e8e113c85a1a6a769e392c940b
Scott Wright <scott.wright.uk@gmail.com>
2023-09-21 15:50:44 +0100

    add: basic api call to fetch data
    This is a simple function to get a feel for playing around with the API and its data and async/await syntax.
    
    To be refactored later.

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,1 +1,29 @@
-console.log("app.js file is connected");
+// Write the functions that hit the API. You’re going to want functions that can take a location and return the weather data for that location. For now, just console.log() the information.
+
+async function getWeatherData() {
+    try {
+        const response = await fetch("https://api.weatherapi.com/v1/current.json?key=dd41ad9a0fc4482f91085416231509&q=nyc&aqi=no", { mode: 'cors' });
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const fetchedData = await response.json();
+
+        if (!fetchedData) {
+            throw new Error("No Data was fetched from your API call");
+        }
+        return fetchedData;
+    } catch (error) {
+        console.log("There was an error fetching the data:", error);
+        throw error;
+    }
+}
+
+getWeatherData()
+    .then(data => {
+        console.log(`The current temperature in ${data.location.name} is ${data.current.temp_c} degrees and the outlook is ${data.current.condition.text}`);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
