18c09e70fe6517859db8a970fa3c2c48b00fad89
Scott Wright <scott.wright.uk@gmail.com>
2023-09-25 16:54:51 +0100

    refactor: Rename 'search' to 'query'
    As per the HTML refactor, this commit updates all references of 'search' to 'query' to align with the naming convention used in the WeatherAPI, improving code readability.

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,13 +1,12 @@
-async function fetchWeatherData(locationSearch) {
+async function fetchWeatherData(locationQuery) {
     try {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=dd41ad9a0fc4482f91085416231509&q=${locationSearch}&aqi=no`, { mode: 'cors' });
-        
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=dd41ad9a0fc4482f91085416231509&q=${locationQuery}&aqi=no`, { mode: 'cors' });
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         const fetchedData = await response.json();
-
+        console.log('fetchedData', fetchedData);
         if (!fetchedData) {
             throw new Error("No Data was fetched from your API call");
         }
@@ -18,13 +17,13 @@ async function fetchWeatherData(locationSearch) {
     }
 }
 
-const searchForm = document.querySelector('#location-search');
+const searchForm = document.querySelector('#location-query');
 
 searchForm.addEventListener('submit', event => {
     event.preventDefault();
 
-    const locationSearch = document.querySelector('#search-input').value;
-    fetchWeatherData(locationSearch.trim())
+    const locationQuery = document.querySelector('#location-query-input').value;
+    fetchWeatherData(locationQuery.trim())
     .then(data => {
         console.log(`The current temperature in ${data.location.name} is ${data.current.temp_c} degrees and the outlook is ${data.current.condition.text}`);
     })
