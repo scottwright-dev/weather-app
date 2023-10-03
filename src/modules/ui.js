a8b30a95aaab91af877d8816ea32113ba3184e55
Scott Wright <scott.wright.uk@gmail.com>
2023-10-03 13:36:45 +0100

    feat(ui): display weather data in the UI
    - Replace previous console logs with actual UI display for weather data.
    - This initial implementation provides a visual representation of the weather data for users.

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,10 +1,15 @@
 export function updateUI(data) {
-    console.log(`The current temperature in ${data.locationName} is ${data.currentTemp} degrees, and the outlook is ${data.currentCondition}. 
+    const displayData = document.querySelector('.display-data');
 
-        The forecast for the next 3 days is:
+    const content = `
+        <p>The current temperature in ${data.locationName} is ${data.currentTemp} degrees, and the outlook is ${data.currentCondition}.</p>
+        <p>The forecast for the next 3 days is:</p>
+        <ul>
+            <li>${data.forecast[0].date} will be ${data.forecast[0].condition}</li>
+            <li>${data.forecast[1].date} will be ${data.forecast[1].condition}</li>
+            <li>${data.forecast[2].date} will be ${data.forecast[2].condition}</li>
+        </ul>
+    `;
 
-        ${data.forecast[0].date} will be ${data.forecast[0].condition}
-        ${data.forecast[1].date} will be ${data.forecast[1].condition} 
-        ${data.forecast[2].date} will be ${data.forecast[2].condition} 
-    `);
+    displayData.innerHTML = content;
 }
