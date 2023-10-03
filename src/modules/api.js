707655361a6a223246b484d09eb232d1776ce478
Scott Wright <scott.wright.uk@gmail.com>
2023-10-03 12:53:01 +0100

    feat: data extraction in fetchWeatherData function
    This commit provides a more detailed extraction of weather data from the API response.
    The focus here has been to identify the raw data values that the application will use for building its UI components.
    In future commits, the intent is to further refine these extractions by splitting them into separate, manageable functions for better modularity and clarity.

--- src/modules/api.js
+++ src/modules/api.js
@@ -23,11 +23,27 @@ export async function fetchWeatherData(locationQuery) {
 
     return {
         locationName: fetchedData.location.name,
-        currentTemp: fetchedData.current.temp_c,
+        locationRegion: fetchedData.location.region,
+        locationCountry: fetchedData.location.country,
+        locationLocalTime: fetchedData.location.localtime,
+
         currentCondition: fetchedData.current.condition.text,
+        currentConditionIcon: fetchedData.current.condition.icon,
+        currentTempC: fetchedData.current.temp_c,
+        feelsLikeC: fetchedData.current.feelslike_c,
+        currentTempF: fetchedData.current.temp_f,
+        feelsLikeF: fetchedData.current.feelslike_f,
+        humidity: fetchedData.current.humidity,
+        windKph: fetchedData.current.wind_kph,
+        windMph: fetchedData.current.wind_mph,
+
         forecast: fetchedData.forecast.forecastday.map(day => ({
             date: day.date,
             condition: day.day.condition.text,
+            icon: day.day.condition.icon,
+            avgTempC: day.day.avgtemp_c,
+            avgTempF: day.day.avgtemp_f,
+            rainChance: day.day.daily_chance_of_rain,
         })),
     };
 }
