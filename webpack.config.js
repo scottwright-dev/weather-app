d7755bf2cc311efb039fceb19fe3bc5cce277970
Scott Wright <scott.wright.uk@gmail.com>
2023-10-23 10:58:14 +0100

    add: HtmlWebpackPlugin and update associated files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -31,4 +32,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
 };
