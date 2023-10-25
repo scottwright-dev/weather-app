1752cb9d87374233e9f004773ab4a55def1ac643
Scott Wright <scott.wright.uk@gmail.com>
2023-10-25 17:27:11 +0100

    fix: update tailwind.config content path for dev
    - This commit changes the content array to target the source files in the src directory instead of the built files in the dist directory.
    
    - Reason: This was an error in my initial set up.
    The change ensures Tailwind purges and includes the necessary styles during development, based on the source files being worked on rather than the build files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html", "./src/index.js", "./src/modules/*.js"],
+  content: ["./src/*.html", "./src/index.js", "./src/modules/*.js"],
   theme: {
     extend: {},
   },
